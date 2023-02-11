9a8af9e789853ea20302717bfca74ff2b2839dcc
daptanque <daniel.apeixoto.tanque@gmail.com>
2023-02-11 11:04:00 +0000

    First Commit of SpringBoot BE part

---
+++ src/main/java/org/coderpt/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.coderpt.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
